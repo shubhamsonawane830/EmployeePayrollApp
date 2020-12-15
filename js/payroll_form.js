74a2731a69c950f553bbb576210a5222f5b7f440
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-15 19:39:06 +0530

    Added: function to store objects into local storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -26,10 +26,23 @@ window.addEventListener('DOMContentLoaded', (event) => {
 const save = () => {
     try {
         let employeePayrollData = createEmployeePayroll();
+        createAndUpdateStorage(employeePayrollData);
     } catch (e) {
         return;
     }
 }
+
+function createAndUpdateStorage(employeePayrollData){
+     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+      if(employeePayrollList != undefined){
+           employeePayrollList.push(employeePayrollData);
+    } else{
+         employeePayrollList = [employeePayrollData]
+}
+alert(employeePayrollList.toString());
+localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList))
+}
+
 
 const createEmployeePayroll = () => {
     let employeePayrollData = new EmployeePayrollData();
