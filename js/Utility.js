c023d01b717f3b3e26c71eb3af91bee00e3478a3
shubhamsonawane830 <shubhamsonawane830@gmail.com>
2020-12-19 11:41:45 +0530

    update method to redirect from home page to form

--- js/Utility.js
+++ js/Utility.js
@@ -3,3 +3,10 @@ const stringyfyDate = (date) => {
     const newDate = !date ? "undefined" : new Date(Date.parse(date)).toLocaleDateString('en-GB', options);
     return newDate;
 }
+
+const update = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._name == node.id);
+    if(!empPayrollData) return;
+    localStorage.setItem('editEmp', JSON.stringify(empPayrollData))
+    window.location.replace(site_properties.add_emp_payroll_page);
+}
